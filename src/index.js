93f80f03d78c19df2cd7f36e0769dd1d9e252498
wlukla <vladislav.danilochkin@gmail.com>
2019-09-23 01:58:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let regex = '';
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (isNaN(+(bracketsConfig[0][0])) !== true) {
+      regex += bracketsConfig[i][0] + bracketsConfig[i][1] + '\|';
+    } else {
+      regex += '\\' + bracketsConfig[i][0] + '\\' + bracketsConfig[i][1] + '\|';
+    }
+  }
+  regex = RegExp(regex.slice(0, -1), 'g')
+  let str_length = str.length;
+  let new_length = 0;
+  while (str_length != new_length) {
+    str_length = str.length;
+    str = str.replace(regex, '');
+    new_length = str.length;
+  }
+  return str.length == 0 ? true : false;
 }
